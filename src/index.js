aa30ad836eab3f8ee5def3e927a02cb11877e491
Gatukgl <gatukgl@gmail.com>
2017-03-16 16:00:23 +0700

    Create basic redux app with reducer hot loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Counter from './components/Counter'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './reducers'
+import Counter from './containers/Counter'
 import './style.css'
 
+const initialState = { counter: 100 }
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+) // will create a store in root
+
+if (module.hot) {
+  module.hot.accept('./reducers', () => {
+    const nextRootReducer = require('./reducers').default
+    store.replaceReducer(nextRootReducer)
+  })
+}
+
 ReactDOM.render(
-  <Counter label="I'm a counter" />,
+  <Provider store={store}>
+    <Counter label="I'm a counter" />
+  </Provider>,
   document.getElementById('react-root')
 )
